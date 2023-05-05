592a4f68a3d0b0a43b8396f38098be44ab062901
capyblue <91384341+capyblue@users.noreply.github.com>
2023-05-06 01:52:29 +0900

    Update 자연수 뒤집어 배열로 만들기.java

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.java
@@ -5,7 +5,6 @@ class Solution {
         for(int i=0;i<tmp.length();i++){
             arr[i]=(int)(n%10);
             n/=10;
-            if(n==0) break;
         }
         
         int [] answer = new int[tmp.length()];
